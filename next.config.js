05d69b49e6757b6761901cebfd7bd462db2f4ef9
umerali06 <umerali7454@gmail.com>
2025-08-23 16:23:49 +0500

    new error is resolving

--- next.config.js
+++ next.config.js
@@ -4,18 +4,18 @@ const nextConfig = {
     esmExternals: false,
     forceSwcTransforms: false,
   },
-
-  // Enable SWC-based minification for smaller & faster builds
   swcMinify: true,
-
-  // React best practices
   reactStrictMode: true,
-
-  // Enable compression for faster responses
   compress: true,
-
-  // Remove "x-powered-by: Next.js" header for security
   poweredByHeader: false,
+
+  webpack: (config) => {
+    // Forcefully remove the trace plugin
+    config.plugins = config.plugins.filter(
+      (plugin) => plugin.constructor.name !== 'TraceEntryPointsPlugin'
+    )
+    return config
+  },
 }
 
 module.exports = nextConfig
